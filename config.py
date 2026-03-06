79d034a76459e164879d5995500210dd3687f17c
devopscomputer <pauloti.engenharia@gmail.com>
2026-03-05 22:17:22 -0300

    update: update config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+﻿import os
+
+
+class SniperConfig:
+    """Configurações do Sniper Mode"""
+
+    SNIPER_WAF_BYPASS = os.getenv("SNIPER_WAF_BYPASS", "true").lower() == "true"
+    SNIPER_WAF_VENDOR_HINT = os.getenv("SNIPER_WAF_VENDOR_HINT", "auto")
+    SNIPER_WAF_VARIANTS_PER_VECTOR = int(os.getenv("SNIPER_WAF_VARIANTS_PER_VECTOR", "6"))
+    SNIPER_MIN_DELAY = int(os.getenv("SNIPER_MIN_DELAY", "100"))
+    SNIPER_MAX_DELAY = int(os.getenv("SNIPER_MAX_DELAY", "500"))
+    SNIPER_PROXY_LIST = [p for p in os.getenv("SNIPER_PROXY_LIST", "").split(",") if p]
+    SNIPER_ROTATE_ON_BLOCKS = int(os.getenv("SNIPER_ROTATE_ON_BLOCKS", "3"))
+
